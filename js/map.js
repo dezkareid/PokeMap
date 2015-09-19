b0f01d3cbced7212b09cd5866e3a63f649462335
Joel Humberto Gómez Paredes <elmaildeldezkareid@gmail.com>
2015-09-19 01:13:19 -0500

    Integrated DMaps and removed, line and marker trainer, trainer now is a control map

--- js/map.js
+++ js/map.js
@@ -1,4 +1,5 @@
-var map,line, marker,index = 0,route = [],move, pokeMarkers = [];
+var pokeMarkers = [];
+var map;
 var pokemons = [
   { name: "Bulbasaur", position: {lat: 19.541980, lng: -96.927581} },
   { name: "Yvisaur", position: {lat: 20.223488, lng: -97.956551} },
@@ -10,33 +11,12 @@ var pokemons = [
   { name: "Warturtle", position: {lat: 19.703085, lng: -96.255126} },
   { name: "Blastoise", position: {lat: 19.081306, lng: -95.826660} }
 ];
-var clickPokemon;
+var redControl,clickPokemon;
 
 function initMap() {
-  route.push(new google.maps.LatLng(18.157838, -95.187389));
-  route.push(new google.maps.LatLng(25.657526,-100.36677400000002));
-  
-
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: route[0],
-    zoom: 4,
-    mapTypeId: google.maps.MapTypeId.TERRAIN
-  });
-
-  marker = new google.maps.Marker({
-    position: route[0],
-    icon: 'img/trainer.png',
-    map: map
-  });
-
-
-  line = new google.maps.Polyline({
-    path: route,
-    map: map
-  });
-
-  move = movingAction();
-
+  map = myMap.getMap();
+  map.setZoom(4);
+  myMap.removeControls()
   clickPokemon = function () {
     alert('Atrapaste a un '+this.getTitle());
     this.active = false;
@@ -45,50 +25,36 @@ function initMap() {
 
   createPokemons();
 
-  map.addListener('bounds_changed', function() {
+  myMap.addEvent('bounds_changed', function() {
     showMarkers();
   });
+  addRedControl();
 
-  $(document).keydown(key);
+  myMap.setMapStyle("APPLE");
 }
 
-function key (e) {
-  console.log(e);
-  switch(e.which) {
-    case 37: // left
-      break;
-
-    case 38: // up
-      move();
-      e.preventDefault();
-      break;
-
-    case 39: // right
-      break;
-
-    case 40: // down
-      break;
-
-    default: return; // exit this handler for other keys
-  }
-   
+function addRedControl () {
+  redControl = document.createElement('img');
+  redControl.src = 'img/trainer.png';
+  redControl.index = 1;
+  myMap.getMap().controls[google.maps.ControlPosition.BOTTOM_LEFT].push(redControl);
 }
 
 function createPokemons () {
+  var pokeMarker;
   for (var i = 0; i < pokemons.length; i++) {
-    pokeMarkers[i] = new google.maps.Marker({map: map, position: pokemons[i].position, visible:false});
-    pokeMarkers[i].setTitle(pokemons[i].name);
-    pokeMarkers[i].setIcon("img/"+(i+1)+".png");
-    pokeMarkers[i].active = true;
-    pokeMarkers[i].addListener('click',clickPokemon);
+    pokeMarker = myMap.addMarker(pokemons[i].position.lat,pokemons[i].position.lng, pokemons[i].name);
+    pokeMarker.setIcon("img/"+(i+1)+".png");
+    pokeMarker.active = true;
+    pokeMarker.addEvent('click',clickPokemon);
+    pokeMarkers.push(pokeMarker);
   }
 }
 
 function showMarkers () {
-  console.log('show');
   var mostrar = map.getZoom() > 7;
   for (var i = 0; i < pokemons.length; i++) {
-    if (map.getBounds().contains(pokeMarkers[i].getPosition())) {
+    if (myMap.getMap().getBounds().contains(pokeMarkers[i].getPosition())) {
       if (pokeMarkers[i].active){
         pokeMarkers[i].setVisible(mostrar);
       }
@@ -97,19 +63,8 @@ function showMarkers () {
   }
 }
 
-function movingAction () {
-  var step = 0;
-  var numSteps = 100;
-  return function(value) {
-    step = value || step+1;
-    if (step>numSteps){
-      return;
-    }
-    marker.setPosition(google.maps.geometry.spherical.interpolate(route[index],route[index+1],step/numSteps));
-    map.setCenter(marker.getPosition());
-    console.log(map.getCenter());
-  };
-  
-}
+var myMap = new DMaps(18.157838, -95.187389,'map',initMap);
+
+
 
 
